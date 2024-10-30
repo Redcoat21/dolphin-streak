989ab8e26f9cba87b4ee9337006cd031a628991f
Dahren Ericsson <dahrenericsson@proton.me>
2024-10-30 16:56:22 +0700

    chore: Configure CORS for localhost:3000 (The NextJS app).
    If it needed to change later, please contact me

--- dolphin-streak-backend/src/main.ts
+++ dolphin-streak-backend/src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      credentials: true,
+      origin: "http://localhost:3000",
+    },
+  });
   app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe());
 
