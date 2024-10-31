1effe31fc5928c172bcbddd711dc8045703433cd
Dahren Ericsson <dahrenericsson@proton.me>
2024-10-31 22:43:05 +0700

    chore(user): Making small changes where find one, delete, and update no longer accept the user's email, only the object id.
    User CRUD should be done, as it turns out there is no bug in the
    previous commit.

--- dolphin-streak-backend/src/users/users.controller.ts
+++ dolphin-streak-backend/src/users/users.controller.ts
@@ -7,15 +7,15 @@ import {
   Patch,
   Post,
   Query,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import * as argon2 from 'argon2';
-import { FindOneByIdParam } from './dto/find-one-by-id.param';
-import { FindUserQuery } from './dto/find-user.query';
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import * as argon2 from "argon2";
+import { FindOneByIdParam } from "./dto/find-one-by-id.param";
+import { FindUserQuery } from "./dto/find-user.query";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
@@ -34,21 +34,20 @@ export class UsersController {
     return await this.usersService.findAll(queryParam);
   }
 
-  // Can be either id or email.
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.usersService.findOne(id);
+  @Get(":id")
+  async findOne(@Param() findOneParam: FindOneByIdParam) {
+    return await this.usersService.findOne(findOneParam.id);
   }
 
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    const a = await this.usersService.update(id, updateUserDto);
-    console.log('hihihihi');
-    console.log(a);
-    return a;
+  @Patch(":id")
+  async update(
+    @Param() findOneParam: FindOneByIdParam,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.usersService.update(findOneParam.id, updateUserDto);
   }
 
-  @Delete(':id')
+  @Delete(":id")
   async remove(@Param() deleteUserParam: FindOneByIdParam) {
     return await this.usersService.remove(deleteUserParam.id);
   }
