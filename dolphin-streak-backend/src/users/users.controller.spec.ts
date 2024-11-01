1710ef4dd0b7dbcff83114ee39a165249814d628
Dahren Ericsson <dahrenericsson@proton.me>
2024-11-01 15:05:19 +0700

    chore(test): Started to work on the user's controller unit test

--- dolphin-streak-backend/src/users/users.controller.spec.ts
+++ dolphin-streak-backend/src/users/users.controller.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
-import { expect } from "vitest";
+import { expect, vi } from "vitest";
 
 describe("UsersController", () => {
   let controller: UsersController;
 
+  const userService = {
+    findAll: vi.fn(),
+    findOne: vi.fn(),
+    remove: vi.fn(),
+    update: vi.fn(),
+    create: vi.fn(),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [{ provide: UsersService, useValue: userService }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
