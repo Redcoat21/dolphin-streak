b9a6260e6c42a2f95801ca372be2beed0a682dd6
Dahren Ericsson <dahrenericsson@proton.me>
2024-11-09 17:02:49 +0700

    chore(exception): Modified the BadRequest response message to be either an array or a string instead of always a string.

--- dolphin-streak-backend/src/lib/dto/base-create-user.dto.ts
+++ dolphin-streak-backend/src/lib/dto/base-create-user.dto.ts
@@ -11,22 +11,47 @@ import {
 } from "class-validator";
 import { Type } from "class-transformer";
 import { DateTime } from "luxon";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class BaseCreateUserDto {
+    @ApiProperty({
+        description: "The user's first name",
+        example: "John",
+        nullable: false,
+        required: true,
+    })
     @IsNotEmpty()
     @IsDefined()
     @IsString()
     firstName: string;
 
+    @ApiProperty({
+        description: "The user's last name",
+        example: "Doe",
+        nullable: true,
+        required: false,
+    })
     @IsOptional()
     @IsString()
     lastName?: string;
 
+    @ApiProperty({
+        description: "The user's email",
+        example: "john@email.com",
+        nullable: false,
+        required: true
+    })
     @IsNotEmpty()
     @IsDefined()
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        description: "The user's password, it should be a strong password, meaning it should contain at least 8 characters, 2 lowercase letters, 2 uppercase letters, 2 numbers, and 2 symbols",
+        example: "Password123!@",
+        nullable: false,
+        required: true,
+    })
     @IsNotEmpty()
     @IsDefined()
     @IsStrongPassword({
@@ -38,9 +63,21 @@ export class BaseCreateUserDto {
     })
     password: string;
 
+    @ApiProperty({
+        description: "The user's profile picture, it should be a valid URL",
+        example: "https://example.com/profile-picture.jpg",
+        nullable: false,
+        required: true,
+    })
     @IsUrl()
     profilePicture: string;
 
+    @ApiProperty({
+        description: "The user's birth date, the given date should be at least 5 years ago, the given string date should be a valid ISO 8601 date",
+        example: "1996-01-01",
+        nullable: true,
+        required: false,
+    })
     @IsOptional()
     @IsDate()
     @Type(() => Date)
