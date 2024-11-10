2334aa7100acc58e8b5ccfa862f2650b6bc1eb6c
Dahren Ericsson <dahrenericsson@proton.me>
2024-11-10 19:04:51 +0700

    feat(language): Language's CRUD should be done.

--- dolphin-streak-backend/src/languages/languages.controller.ts
+++ dolphin-streak-backend/src/languages/languages.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { LanguagesService } from './languages.service';
-import { CreateLanguageDto } from './dto/create-language.dto';
-import { UpdateLanguageDto } from './dto/update-language.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
+import { LanguagesService } from "./languages.service";
+import { CreateLanguageDto } from "./dto/create-language.dto";
+import { UpdateLanguageDto } from "./dto/update-language.dto";
+import { FindByIdParam } from "src/lib/dto/find-by-id-param.dto";
 
-@Controller('languages')
+@Controller("languages")
 export class LanguagesController {
   constructor(private readonly languagesService: LanguagesService) {}
 
@@ -17,18 +26,21 @@ export class LanguagesController {
     return this.languagesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.languagesService.findOne(+id);
+  @Get(":id")
+  findOne(@Param() findByIdParam: FindByIdParam) {
+    return this.languagesService.findOne(findByIdParam.id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLanguageDto: UpdateLanguageDto) {
-    return this.languagesService.update(+id, updateLanguageDto);
+  @Patch(":id")
+  update(
+    @Param() findByIdParam: FindByIdParam,
+    @Body() updateLanguageDto: UpdateLanguageDto,
+  ) {
+    return this.languagesService.update(findByIdParam.id, updateLanguageDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.languagesService.remove(+id);
+  @Delete(":id")
+  remove(@Param() findByIdParam: FindByIdParam) {
+    return this.languagesService.remove(findByIdParam.id);
   }
 }
--- dolphin-streak-backend/src/languages/languages.service.ts
+++ dolphin-streak-backend/src/languages/languages.service.ts
@@ -1,26 +1,74 @@
-import { Injectable } from '@nestjs/common';
-import { CreateLanguageDto } from './dto/create-language.dto';
-import { UpdateLanguageDto } from './dto/update-language.dto';
+import { Injectable } from "@nestjs/common";
+import { CreateLanguageDto } from "./dto/create-language.dto";
+import { UpdateLanguageDto } from "./dto/update-language.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Language } from "./schemas/language.schema";
+import { FilterQuery, Model, ProjectionType, QueryOptions } from "mongoose";
 
 @Injectable()
+/**
+ * Service for managing languages.
+ */
 export class LanguagesService {
+  /**
+   * Constructs the LanguagesService.
+   * @param languageModel - The language model injected by Mongoose.
+   */
+  constructor(
+    @InjectModel(Language.name) private languageModel: Model<Language>,
+  ) {}
+
+  /**
+   * Creates a new language document.
+   * @param createLanguageDto - Data transfer object for creating a language.
+   * @returns The created language document.
+   */
   create(createLanguageDto: CreateLanguageDto) {
-    return 'This action adds a new language';
+    return this.languageModel.create(createLanguageDto);
   }
 
-  findAll() {
-    return `This action returns all languages`;
+  /**
+   * Finds all language documents matching the query.
+   * @param query - The filter query to match documents.
+   * @param projection - Optional projection to specify which fields to include or exclude.
+   * @param options - Optional query options.
+   * @returns An array of language documents.
+   */
+  findAll(
+    query?: FilterQuery<Language>,
+    projection?: ProjectionType<Language>,
+    options?: QueryOptions<Language>,
+  ) {
+    return this.languageModel.find(query, projection, options);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} language`;
+  /**
+   * Finds a single language document by its ID.
+   * @param id - The ID of the language document to find.
+   * @returns The found language document, or null if no document matches the ID.
+   */
+  findOne(id: string) {
+    return this.languageModel.findById(id);
   }
 
-  update(id: number, updateLanguageDto: UpdateLanguageDto) {
-    return `This action updates a #${id} language`;
+  /**
+   * Updates a language document by its ID.
+   * @param id - The ID of the language document to update.
+   * @param updateLanguageDto - Data transfer object for updating a language.
+   * @returns The updated language document.
+   */
+  update(id: string, updateLanguageDto: UpdateLanguageDto) {
+    return this.languageModel.findByIdAndUpdate(id, updateLanguageDto, {
+      new: true,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} language`;
+  /**
+   * Removes a language document by its ID.
+   * @param id - The ID of the language document to remove.
+   * @returns The removed language document.
+   */
+  remove(id: string) {
+    return this.languageModel.findByIdAndDelete(id);
   }
 }
