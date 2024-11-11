68cb678de96606feacd805284631f7af460307e5
Dahren Ericsson <dahrenericsson@proton.me>
2024-11-12 00:06:42 +0700

    documentation(language): Added documentation to language controller

--- dolphin-streak-backend/src/languages/languages.controller.ts
+++ dolphin-streak-backend/src/languages/languages.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   HttpException,
   HttpStatus,
   Param,
@@ -19,13 +20,79 @@ import { Role } from "src/users/schemas/user.schema";
 import { RoleGuard } from "src/lib/guard/role.guard";
 import { JwtAuthGuard } from "src/auth/guard/jwt-auth.guard";
 import { checkIfExist, formatGetAllMessages } from "src/lib/utils/response";
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from "@nestjs/swagger";
 
 @Controller("/api/languages")
 @UseGuards(JwtAuthGuard, RoleGuard)
 @HasRoles(Role.ADMIN)
+@ApiUnauthorizedResponse({
+  description:
+    "Happen because the user is not authorized (doesn't have a valid access token)",
+  example: {
+    message: "Unauthorized",
+    data: null,
+  },
+})
+@ApiForbiddenResponse({
+  description:
+    "Happen because the user doesn't have the right role to access this endpoint",
+  example: {
+    message: "Forbidden resource",
+    data: null,
+  },
+})
+@ApiInternalServerErrorResponse({
+  description:
+    "Happen when something went wrong, that is not handled by this API, e.g. database error",
+  example: {
+    message: "Internal Server Error",
+    data: null,
+  },
+})
 export class LanguagesController {
   constructor(private readonly languagesService: LanguagesService) {}
 
+  @ApiOperation({
+    summary: "Create a new language",
+    description: "This endpoint will create a new language",
+  })
+  @ApiCreatedResponse({
+    description: "Language created successfully",
+    example: {
+      messages: "Language created successfully",
+      data: {
+        _id: "612a4b7e8c4e2c001f1f8c5a",
+        name: "English",
+        image:
+          "https://cdn.countryflags.com/thumbs/united-states-of-america/flag-3d-round-250.png",
+        __v: 0,
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: "Happen when the request body is not valid",
+    example: {
+      messages: [
+        "name should not be empty",
+        "name must be a string",
+        "name must be longer than or equal to 3 characters",
+        "image should not be empty",
+        "image must be a string",
+        "image must be longer than or equal to 3 characters",
+      ],
+      data: null,
+    },
+  })
+  @HttpCode(HttpStatus.CREATED)
   @Post()
   async create(@Body() createLanguageDto: CreateLanguageDto) {
     return {
@@ -34,6 +101,25 @@ export class LanguagesController {
     };
   }
 
+  @ApiOperation({
+    summary: "Get all languages",
+    description: "This endpoint will return all languages",
+  })
+  @ApiOkResponse({
+    description: "Languages founded",
+    example: {
+      messages: "Successfully get all languages",
+      data: [
+        {
+          _id: "612a4b7e8c4e2c001f1f8c5a",
+          name: "English",
+          image:
+            "https://cdn.countryflags.com/thumbs/united-states-of-america/flag-3d-round-250.png",
+          __v: 0,
+        },
+      ],
+    },
+  })
   @Get()
   async findAll() {
     const foundedLanguages = await this.languagesService.findAll();
@@ -43,6 +129,39 @@ export class LanguagesController {
     };
   }
 
+  @ApiOperation({
+    summary: "Get one language",
+    description: "This endpoint will return one language",
+  })
+  @ApiOkResponse({
+    description: "Language founded",
+    example: {
+      messages: "Language founded",
+      data: {
+        _id: "612a4b7e8c4e2c001f1f8c5a",
+        name: "English",
+        image:
+          "https://cdn.countryflags.com/thumbs/united-states-of-america/flag-3d-round-250.png",
+        __v: 0,
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: "Happen when the id parameter is not a valid mongodb id",
+    example: {
+      messages: [
+        "id must be a mongodb id",
+      ],
+      data: null,
+    },
+  })
+  @ApiNotFoundResponse({
+    description: "Happen when the language is not found",
+    example: {
+      messages: "Language not found",
+      data: null,
+    },
+  })
   @Get(":id")
   async findOne(@Param() findByIdParam: FindByIdParam) {
     const foundedLanguage = checkIfExist(
@@ -58,6 +177,40 @@ export class LanguagesController {
     };
   }
 
+  //TODO: Need to check if update is empty, will it still update?
+  @ApiOperation({
+    summary: "Update a language",
+    description: "This endpoint will update a language",
+  })
+  @ApiOkResponse({
+    description: "Language updated successfully",
+    example: {
+      messages: "Language updated successfully",
+      data: {
+        _id: "612a4b7e8c4e2c001f1f8c5a",
+        name: "English",
+        image:
+          "https://cdn.countryflags.com/thumbs/united-states-of-america/flag-3d-round-250.png",
+        __v: 0,
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: "Happen when the given id is not a valid mongodb id",
+    example: {
+      messages: [
+        "id must be a mongodb id",
+      ],
+      data: null,
+    },
+  })
+  @ApiNotFoundResponse({
+    description: "Happen when the language is not found",
+    example: {
+      messages: "Language not found",
+      data: null,
+    },
+  })
   @Patch(":id")
   async update(
     @Param() findByIdParam: FindByIdParam,
@@ -77,6 +230,39 @@ export class LanguagesController {
     };
   }
 
+  @ApiOperation({
+    summary: "Delete a language",
+    description: "This endpoint will delete a language",
+  })
+  @ApiOkResponse({
+    description: "Language deleted successfully",
+    example: {
+      messages: "Language deleted successfully",
+      data: {
+        _id: "612a4b7e8c4e2c001f1f8c5a",
+        name: "English",
+        image:
+          "https://cdn.countryflags.com/thumbs/united-states-of-america/flag-3d-round-250.png",
+        __v: 0,
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: "Happen when the given id is not a valid mongodb id",
+    example: {
+      messages: [
+        "id must be a mongodb id",
+      ],
+      data: null,
+    },
+  })
+  @ApiNotFoundResponse({
+    description: "Happen when the language is not found",
+    example: {
+      messages: "Language not found",
+      data: null,
+    },
+  })
   @Delete(":id")
   async remove(@Param() findByIdParam: FindByIdParam) {
     const deletedLanguage = checkIfExist(
