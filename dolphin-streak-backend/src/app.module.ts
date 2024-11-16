9dfd55d1d5b994900efc40eca44ab66a539f290b
Dahren Ericsson <dahrenericsson@proton.me>
2024-11-17 01:53:47 +0700

    chore(app): Added the MailModule at the app.module.ts

--- dolphin-streak-backend/src/app.module.ts
+++ dolphin-streak-backend/src/app.module.ts
@@ -1,17 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ScheduleModule } from '@nestjs/schedule';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { SubscriptionsModule } from './subscriptions/subscriptions.module';
-import { QuestionsModule } from './questions/questions.module';
-import { CoursesModule } from './courses/courses.module';
-import { LanguagesModule } from './languages/languages.module';
-import { LevelsModule } from './levels/levels.module';
-import { ViewController } from './views/view.controller'; // Import the new views controller
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ScheduleModule } from "@nestjs/schedule";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { SubscriptionsModule } from "./subscriptions/subscriptions.module";
+import { QuestionsModule } from "./questions/questions.module";
+import { CoursesModule } from "./courses/courses.module";
+import { LanguagesModule } from "./languages/languages.module";
+import { LevelsModule } from "./levels/levels.module";
+import { ViewController } from "./views/view.controller"; // Import the new views controller
+import { MailController } from "./mail/mail.controller";
+import { MailService } from "./mail/mail.service";
+import { MailModule } from "./mail/mail.module";
 
 @Module({
   imports: [
@@ -22,7 +25,7 @@ import { ViewController } from './views/view.controller'; // Import the new view
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('DB_URI'),
+        uri: configService.get<string>("DB_URI"),
       }),
     }),
     ScheduleModule.forRoot(),
@@ -33,6 +36,7 @@ import { ViewController } from './views/view.controller'; // Import the new view
     CoursesModule,
     LanguagesModule,
     LevelsModule,
+    MailModule,
   ],
   controllers: [AppController, ViewController],
   providers: [AppService],
