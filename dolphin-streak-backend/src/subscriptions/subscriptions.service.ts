db8851463d0795ba400324012439dd0bd6c6396c
RedFrosy <112476864+RedFrosy@users.noreply.github.com>
2024-11-28 23:30:41 +0700

    fix minor typo

--- dolphin-streak-backend/src/subscriptions/subscriptions.service.ts
+++ dolphin-streak-backend/src/subscriptions/subscriptions.service.ts
@@ -7,30 +7,6 @@ import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 import { User } from 'src/users/schemas/user.schema';
 
-// @Injectable()
-// export class SubscriptionsService {
-//   create(createSubscriptionDto: CreateSubscriptionDto) {
-//     return 'This action adds a new subscription';
-//   }
-
-//   findAll() {
-//     return `This action returns all subscriptions`;
-//   }
-
-//   findOne(id: number) {
-//     return `This action returns a #${id} subscription`;
-//   }
-
-//   update(id: number, updateSubscriptionDto: UpdateSubscriptionDto) {
-//     return `This action updates a #${id} subscription`;
-//   }
-
-//   remove(id: number) {
-//     return `This action removes a #${id} subscription`;
-//   }
-  
-// }
-
 @Injectable()
 export class SubscriptionsService {
   constructor(
@@ -73,7 +49,7 @@ export class SubscriptionsService {
     // console.log(response.data);
 
     if(!token){
-      return { subscriptionId: nullF };
+      return { subscriptionId: null };
     }
 
     const now = new Date();
