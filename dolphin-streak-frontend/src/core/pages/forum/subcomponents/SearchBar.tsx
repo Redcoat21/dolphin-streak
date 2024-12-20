a19c62a8f984b8456b46355e468477e68983348e
RedFrosy <112476864+RedFrosy@users.noreply.github.com>
2024-12-20 14:52:24 +0700

    add link to add new forum

--- dolphin-streak-frontend/src/core/pages/forum/subcomponents/SearchBar.tsx
+++ dolphin-streak-frontend/src/core/pages/forum/subcomponents/SearchBar.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Plus, Search } from "lucide-react";
+import Link from "next/link";
 
 // Search Bar Component
 interface SearchBarProps {
@@ -18,9 +19,11 @@ export function SearchBar({ onSearch, onNewPost }: SearchBarProps) {
         />
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-gray-400" />
       </div>
-      <Button className="bg-blue-500 hover:bg-blue-600 h-10 w-10 p-0">
-        <Plus className="h-5 w-5" />
-      </Button>
+      <Link href="/forum/new">
+        <Button className="bg-blue-500 hover:bg-blue-600 h-10 w-10 p-0">
+          <Plus className="h-5 w-5" />
+        </Button>
+      </Link>
     </div>
   );
 }
