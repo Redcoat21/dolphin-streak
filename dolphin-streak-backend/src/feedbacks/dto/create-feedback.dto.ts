bf8129ea1f1dc22315a80a0f7416082f2bea03ac
RedFrosy <112476864+RedFrosy@users.noreply.github.com>
2025-01-12 13:20:50 +0700

    using accessToken to create feedback

--- dolphin-streak-backend/src/feedbacks/dto/create-feedback.dto.ts
+++ dolphin-streak-backend/src/feedbacks/dto/create-feedback.dto.ts
@@ -3,13 +3,13 @@ import { FeedbackType } from "../schemas/feedback.schema";
 import { ApiProcessingResponse, ApiProperty } from "@nestjs/swagger";
 
 export class CreateFeedbackDto {
-    @ApiProperty({
-        description: "The user who created this feedback",
-        example: "612a4b7e8c4e2c001f1f8c5a",
-    })
-    @IsMongoId()
-    @IsNotEmpty()
-    user: string;
+    // @ApiProperty({
+    //     description: "The user who created this feedback",
+    //     example: "612a4b7e8c4e2c001f1f8c5a",
+    // })
+    // @IsMongoId()
+    // @IsNotEmpty()
+    // user: string;
     
     @ApiProperty({
         description: "The type of this feedback",
