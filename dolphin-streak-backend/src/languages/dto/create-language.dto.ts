b7a8aed82912daf8522c3aafaa10c2632f3b9778
Dahren Ericsson <dahrenericsson@proton.me>
2025-01-13 22:51:46 +0700

    chore: Updated the image requirement in the create course and language DTO.

--- dolphin-streak-backend/src/languages/dto/create-language.dto.ts
+++ dolphin-streak-backend/src/languages/dto/create-language.dto.ts
@@ -8,13 +8,4 @@ export class CreateLanguageDto {
   })
   @IsString()
   name: string;
-
-  @ApiProperty({
-    description:
-      'The URL of an image representing the language. Image can be uploaded through URL or through PATCH :id/thumbnail',
-    example:
-      'https://cdn.countryflags.com/thumbs/united-states-of-america/flag-3d-round-250.png',
-  })
-  @IsUrl()
-  image: string;
 }
